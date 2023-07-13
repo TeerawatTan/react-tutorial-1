3f71cad96e0b7b63c7c19b85d7e5d8edc3f1d94d
teerawat.t1 <teerawat.tanatan@gmail.com>
2023-07-13 21:29:40 +0700

    Edit title file

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,7 @@
 export default function Title () {
     return (
         <div>
-            <h1 style={{color:"blue",textAlign:"center"}}># บันทึกรายการบัญชีรายรับ-รายจ่าย #</h1>
+            <h1 style={{color:"blue",textAlign:"center"}}>บันทึกรายการบัญชีรายรับ-รายจ่าย</h1>
         </div>
     )
 }
